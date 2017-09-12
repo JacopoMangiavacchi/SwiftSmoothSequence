2459d35853803ad2e71e703517b0c97340fd8553
Jacopo <jmangia@me.com>
2017-09-12 09:29:26 -0700

    Add EWMA method (Exponentially Weighted Moving Average)

--- Sources/SmoothSequence.swift
+++ Sources/SmoothSequence.swift
@@ -7,6 +7,8 @@
 //
 import Foundation
 
+
+// Buffer method
 public struct SequenceSmoother<Element> {
     public typealias elementAddFunc = (Element, Element) -> Element
     public typealias elementDivideFunc = (Element, Int) -> Element
@@ -44,5 +46,42 @@ public struct SequenceSmoother<Element> {
         
         //Return Average
         return divideFunc(cache.reduce(emptyElement, addFunc), cache.count)   //cache.reduce(0, +) / cache.count
+    }
+}
+
+
+
+// Exponentially weighted moving average
+public struct EWMA<Element> {
+    public typealias elementAddFunc = (Element, Element) -> Element
+    public typealias elementMultiplyFunc = (Element, Float) -> Element
+    
+    fileprivate var last: Element?
+    fileprivate var smooth: Float!
+    fileprivate var addFunc: elementAddFunc!
+    fileprivate var multiplyFunc: elementMultiplyFunc!
+    
+    
+    public init(smooth: Float = 0.9, addFunc: @escaping elementAddFunc, multiplyFunc: @escaping elementMultiplyFunc) {
+        self.smooth = smooth
+        self.addFunc = addFunc
+        self.multiplyFunc = multiplyFunc
+    }
+    
+    public mutating func reset() {
+        last = nil
+    }
+    
+    public mutating func smooth(_ value: Element) -> Element {
+        if let l = last {
+            // Formula: St = aXt + (1-a)St-1
+            // (smooth * value) + ((1.0-smooth) * l)
+            last = addFunc(multiplyFunc(value, smooth), multiplyFunc(l, 1.0-smooth))    
+            return last!
+        }
+        else {
+            last = value
+            return value
+        }
     }
 }
